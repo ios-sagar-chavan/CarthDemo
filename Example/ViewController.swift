c78979306fa9bfee3d3dbf10e0d9f02bbb27e5a6
Sagar C <sagar.chavan@bnt-soft.com>
2020-02-26 12:50:11 +0530

    first commit

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Sagar C on 25/02/20.
+//  Copyright © 2020 BNT SOFT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ GSKit/GSViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GSViewController.swift
+//  GSKit
+//
+//  Created by Sagar C on 25/02/20.
+//  Copyright © 2020 BNT SOFT. All rights reserved.
+//
+
+import UIKit
+
+class GSViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
